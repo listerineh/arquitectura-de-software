f0ecb3cc60c029aaed4dbc3c9ec710fb809f01ab
listerineh <jsalvarez4@espe.edu.ec>
2022-11-28 20:19:10 -0500

    add: user services

---
+++ primer-parcial/src/main/java/com/alvarez/examenes/primerparcial/users/service/UserService.java
@@ -0,0 +1,9 @@
+package com.alvarez.examenes.primerparcial.users.service;
+
+import java.util.List;
+
+import com.alvarez.examenes.primerparcial.users.model.User;
+
+public interface UserService {
+    List<User> getAllUsers();
+}
---
+++ primer-parcial/src/main/java/com/alvarez/examenes/primerparcial/users/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.alvarez.examenes.primerparcial.users.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alvarez.examenes.primerparcial.users.model.User;
+import com.alvarez.examenes.primerparcial.users.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+}
