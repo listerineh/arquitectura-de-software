8dd1f94a5454c41a2a7d57c1753c02361358b0ee
listerineh <jsalvarez4@espe.edu.ec>
2022-11-28 20:31:25 -0500

    add: user space controller

---
+++ primer-parcial/src/main/java/com/alvarez/examenes/primerparcial/users/controller/UserSpaceController.java
@@ -0,0 +1,21 @@
+package com.alvarez.examenes.primerparcial.users.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alvarez.examenes.primerparcial.users.service.UserSpaceService;
+
+@RestController
+@RequestMapping("/userspace")
+public class UserSpaceController {
+
+    @Autowired
+    UserSpaceService userSpaceService;
+
+    @GetMapping("/all")
+    public Object getAllUserSpaces() {
+        return this.userSpaceService.getAllUserSpaces();
+    }
+}
