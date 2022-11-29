b429ea3eada64f5ed6fde7bcf75e0d4df9549bab
listerineh <jsalvarez4@espe.edu.ec>
2022-11-28 20:29:56 -0500

    add: user space service

---
+++ primer-parcial/src/main/java/com/alvarez/examenes/primerparcial/users/service/UserSpaceService.java
@@ -0,0 +1,10 @@
+package com.alvarez.examenes.primerparcial.users.service;
+
+import java.util.List;
+
+import com.alvarez.examenes.primerparcial.users.model.UserSpace;
+
+public interface UserSpaceService {
+    List<UserSpace> getAllUserSpaces();
+
+}
---
+++ primer-parcial/src/main/java/com/alvarez/examenes/primerparcial/users/service/UserSpaceServiceImpl.java
@@ -0,0 +1,21 @@
+package com.alvarez.examenes.primerparcial.users.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alvarez.examenes.primerparcial.users.model.UserSpace;
+import com.alvarez.examenes.primerparcial.users.repository.UserSpaceRepository;
+
+@Service
+public class UserSpaceServiceImpl implements UserSpaceService {
+
+    @Autowired
+    UserSpaceRepository userSpaceRepository;
+
+    @Override
+    public List<UserSpace> getAllUserSpaces() {
+        return this.userSpaceRepository.findAll();
+    }
+}
