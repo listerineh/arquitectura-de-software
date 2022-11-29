9a7daa3f64d927c02206a7c4155587f3c1df020f
listerineh <jsalvarez4@espe.edu.ec>
2022-11-28 19:32:48 -0500

    initial project structure

---
+++ src/main/java/com/alvarez/examen/primeparcial/PrimeParcialApplication.java
@@ -0,0 +1,13 @@
+package com.alvarez.examen.primeparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvarez/examen/primeparcial/PrimeParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarez.examen.primeparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
